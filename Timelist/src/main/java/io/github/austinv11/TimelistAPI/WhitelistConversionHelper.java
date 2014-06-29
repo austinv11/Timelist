4147181298b2891f7728cae3218fb977f952a4ad
austinv11 <austinv11@gmail.com>
2014-06-29 09:16:03 -0400

    Added support for whitelist.txt conversion

--- Timelist/src/main/java/io/github/austinv11/TimelistAPI/WhitelistConversionHelper.java
+++ Timelist/src/main/java/io/github/austinv11/TimelistAPI/WhitelistConversionHelper.java
@@ -1,9 +1,13 @@
 package io.github.austinv11.TimelistAPI;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.UUID;
+
+import me.armar.plugins.UUIDManager.UUIDManager;
 
 import org.bukkit.Bukkit;
 import org.json.simple.JSONArray;
@@ -14,26 +18,54 @@ import org.json.simple.parser.ParseException;
 public class WhitelistConversionHelper {
 	private static boolean isTimelist;
 	public static void whitelistToTimelist(){
+		File f = new File("whitelist.txt");
+		File f2 = new File("white-list.txt");
 		JSONParser parser = new JSONParser();
 		String name;
 		String uuid;
-		try {
-			Object obj = parser.parse(new FileReader("whitelist.json"));
-			JSONArray whitelist = (JSONArray) obj;
-			for(int i = 0; i < whitelist.size(); i++){
-				JSONObject json = (JSONObject) whitelist.get(i);
-				uuid = (String) json.get("uuid");
-				name = (String) json.get("name");
-				TimelistHandler.addPlayerRaw(uuid, name, -1);//Pre-whitelisted players are allowed forever
+		if (f.exists()){
+			try {
+				BufferedReader reader = new BufferedReader(new FileReader("whitelist.txt"));
+				String line = null;
+				while ((line = reader.readLine()) != null){
+					uuid = UUIDManager.getUUIDFromPlayer(line).toString();
+					name = line;
+					TimelistHandler.addPlayerRaw(uuid, name, -1);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ParseException e) {
-			e.printStackTrace();
+		}else if (f2.exists()){
+			try {
+				BufferedReader reader = new BufferedReader(new FileReader("white-list.txt"));
+				String line = null;
+				while ((line = reader.readLine()) != null){
+					uuid = UUIDManager.getUUIDFromPlayer(line).toString();
+					name = line;
+					TimelistHandler.addPlayerRaw(uuid, name, -1);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}else{
+			try {
+				Object obj = parser.parse(new FileReader("whitelist.json"));
+				JSONArray whitelist = (JSONArray) obj;
+				for(int i = 0; i < whitelist.size(); i++){
+					JSONObject json = (JSONObject) whitelist.get(i);
+					uuid = (String) json.get("uuid");
+					name = (String) json.get("name");
+					TimelistHandler.addPlayerRaw(uuid, name, -1);//Pre-whitelisted players are allowed forever
+				}
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			isTimelist = true;
 		}
-		isTimelist = true;
 	}
 	public static void timelistToWhitelist(){
 		JSONParser parser = new JSONParser();
